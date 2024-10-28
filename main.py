ca5b2d42057b7bc2411f422d659e820687148529
OlgaD-72024 <yachhik@gmail.com>
2024-10-28 20:20:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 def is_valid_email(email):
     return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    if not is_valid_email(recipient):
-        print(f"Невозможно отправить письмо на адрес {recipient}.")
-    elif not is_valid_email(sender):
+    if not is_valid_email(recipient) or not is_valid_email(sender):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
     elif sender == recipient:
-         print("Нельзя отправить письмо самому себе!")
+        print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
